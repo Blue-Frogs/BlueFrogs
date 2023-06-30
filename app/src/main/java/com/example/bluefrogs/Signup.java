0f211f2a24562f00ffe53b1172234299efbf1136
Jihyun Lee <jlee310@go.pasadena.edu>
2023-06-30 02:39:38 -0700

    Sign up UI

--- app/src/main/java/com/example/bluefrogs/Signup.java
+++ app/src/main/java/com/example/bluefrogs/Signup.java
@@ -3,12 +3,20 @@ package com.example.bluefrogs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
-
+    private TextView usernameTV;
+    private TextView passwordTV;
+    private Button signupB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //signupB = findViewById(R.id.idBsignup);
+        //usernameTV = findViewById(R.id.idETusername);
+        //passwordTV = findViewById(R.id.idETpassword);
     }
 }
